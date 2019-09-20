6e916c669bfb8529900196e07e95327ce3e9e530
Michael Martinez <michael.martinez@arcadiagroup.co.uk>
2019-09-20 17:11:29 +0100

    added notes on SSR and loadable components

---
+++ code/developing/dist/2.js
@@ -0,0 +1,12 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push(
+  [
+    [2],
+    {
+      10: function (o, n, w) {
+        "use strict";
+        w.r(n), n.default="Hello from lazy"
+      }
+    }
+  ]
+);
+//# sourceMappingURL=2.js.map
